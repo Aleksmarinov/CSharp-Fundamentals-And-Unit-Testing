24c94c50b92bf421b0ba43a2320ca0d49bb1133e
AleksMarinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-10-02 10:38:53 +0300

    Implicit and Explicit Conversion in C#

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversation
+            int num = 1235948642;
+            long bigNum = num;
+
+            float myFloat = 13.37F;
+            double myNewDouble = myFloat;
+
+            double myDouble = 13.37;
+            int myInt;
+            // explicit conversation
+            // cast double to int
+            myInt = (int)myDouble;
+
+            // type conversation
+            string myString = myDouble.ToString(); // "13,37"
+            string myNewString = myFloat.ToString();
+            bool sunIsShining = false;
+
+            string myBoolString = sunIsShining.ToString();
+
+            Console.WriteLine(myBoolString);
+            Console.Read();
+        }
+    }
+       
+}
