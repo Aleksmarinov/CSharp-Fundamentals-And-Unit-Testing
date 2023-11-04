116dc74870c21485b36d3532143c6e980b12ae73
AleksMarinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-11-04 18:38:48 +0200

    New-House
    C# if-else statements exercise

--- Program.cs
+++ Program.cs
@@ -1,34 +1,59 @@
-﻿using System;
-
-namespace HelloWorld
+﻿namespace _03.New_House
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            // implicit conversation
-            int num = 1235948642;
-            long bigNum = num;
+            String flowersType = Console.ReadLine();
+            int flowersCount = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+            double cost = 0;
 
-            float myFloat = 13.37F;
-            double myNewDouble = myFloat;
-
-            double myDouble = 13.37;
-            int myInt;
-            // explicit conversation
-            // cast double to int
-            myInt = (int)myDouble;
-
-            // type conversation
-            string myString = myDouble.ToString(); // "13,37"
-            string myNewString = myFloat.ToString();
-            bool sunIsShining = false;
-
-            string myBoolString = sunIsShining.ToString();
-
-            Console.WriteLine(myBoolString);
-            Console.Read();
+            switch (flowersType)
+            {
+                case "Roses":
+                    cost = flowersCount * 5;
+                    if (flowersCount > 80)
+                    {
+                        cost = cost * 0.9;
+                    }
+                    break;
+                case "Dahlias":
+                    cost = flowersCount * 3.8;
+                    if (flowersCount > 90)
+                    {
+                        cost = cost * 0.85;
+                    }
+                    break;
+                case "Tulips":
+                    cost = flowersCount * 2.8;
+                    if (flowersCount > 80)
+                    {
+                        cost = cost * 0.85;
+                    }
+                    break;
+                case "Narcissus":
+                    cost = flowersCount * 3;
+                    if (flowersCount < 120)
+                    {
+                        cost = cost * 1.15;
+                    }
+                    break;
+                case "Gladiolus":
+                    cost = flowersCount * 2.5;
+                    if (flowersCount < 80)
+                    {
+                        cost = cost * 1.2;
+                    }
+                    break;
+            }
+            if (budget < cost)
+            {
+                Console.WriteLine($"Not enough money, you need {(cost - budget):f2} leva more.");
+            } else
+            {
+                Console.WriteLine($"Hey, you have a great garden with {flowersCount} {flowersType} and {(budget - cost):f2} leva left.");
+            }
         }
     }
-       
 }
