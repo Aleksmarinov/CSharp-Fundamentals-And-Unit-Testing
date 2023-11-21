27043d58272f00785396ef2c7deae4e18fd9970a
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-11-21 10:00:07 +0200

    Exam-countdown
    For loops - C# exercise

---
+++ For/For-loops/Program.cs
@@ -0,0 +1,7 @@
+﻿int daysCount = int.Parse(Console.ReadLine());
+
+for (int currentDay = daysCount; currentDay >= 1; currentDay -= 1)
+{
+    Console.WriteLine($"{currentDay} days before the exam");
+}
+Console.WriteLine("The exam has come");
