f967daf992592dbeb5cda6ceac2d7ff80d70868f
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-11-22 12:01:22 +0200

    While loops exercises
    C# while loops basics

---
+++ While-loops-exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = int.Parse(Console.ReadLine()?? "0"); //Read an initial number from the input
+string command = Console.ReadLine(); // Read an execute a sequence of the command
+
+while (command != "End") // if command == "End" --> we end our loop
+{
+    if (command == "Inc") // "Inc" – add 1 to the number (increment)
+    {
+        number ++;
+    }
+    else if (command == "Dec") // "Dec" – subtract 1 from the number (decrement)
+    {
+        number --;
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(number); 
