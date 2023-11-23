70eec0960635ae13b127fef053c6b4a49dcb6bb3
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-11-23 10:02:59 +0200

    Sum-numbers
    While loop C# exercise

--- While-loops-exercise/Program.cs
+++ While-loops-exercise/Program.cs
@@ -1,17 +1,11 @@
-﻿int number = int.Parse(Console.ReadLine()?? "0"); //Read an initial number from the input
-string command = Console.ReadLine(); // Read an execute a sequence of the command
+﻿int n = int.Parse(Console.ReadLine());
+double sum = 0;
+int i = 1;
 
-while (command != "End") // if command == "End" --> we end our loop
+while (i <= n)
 {
-    if (command == "Inc") // "Inc" – add 1 to the number (increment)
-    {
-        number ++;
-    }
-    else if (command == "Dec") // "Dec" – subtract 1 from the number (decrement)
-    {
-        number --;
-    }
-    command = Console.ReadLine();
+    double number = double.Parse(Console.ReadLine());
+    sum += number;
+    i++;
 }
-
-Console.WriteLine(number); 
+Console.WriteLine(sum);
