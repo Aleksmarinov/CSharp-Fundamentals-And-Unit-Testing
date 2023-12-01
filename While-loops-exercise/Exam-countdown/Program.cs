3f0e297a2acc6cc3ffc74aa63276e35cc25b0df9
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-01 10:20:24 +0200

    Exam-countdown
    C# While loop exercise

---
+++ While-loops-exercise/Exam-countdown/Program.cs
@@ -0,0 +1,8 @@
+﻿int daysBeforeExam = int.Parse(Console.ReadLine());
+
+while (daysBeforeExam > 0)
+{
+    Console.WriteLine($"{daysBeforeExam} days before the exam");
+    daysBeforeExam--;
+}
+Console.WriteLine("The exam has come");
