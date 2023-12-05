fa7ea0a94fbdb1c113da5b5e37d595be5d3eadcf
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-05 14:11:32 +0200

    Sum-Numbers
    C# Exercise

---
+++ While-loops-exercise/Sum-Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+double sum = 0;
+int i = 1;
+
+while (i <= n)
+{
+    double number = double.Parse(Console.ReadLine());
+    sum += number;
+    i++;
+}
+Console.WriteLine(sum);
