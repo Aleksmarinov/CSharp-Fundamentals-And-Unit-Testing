01e6512a62251e6bda2557e49aeb5e64d329320f
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-10 14:08:05 +0200

    Triangle of stars
    C# exercise

---
+++ Nested-loops/Triangle-of-stars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _3.__Triangle_of_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+
+            for (int row = 1; row <= n; row += 1)
+            {
+                for (int col = 1; col <= n; col += 1)
+                {
+                    Console.Write("* ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
