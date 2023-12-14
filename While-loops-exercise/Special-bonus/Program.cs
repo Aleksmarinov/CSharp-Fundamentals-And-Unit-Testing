bda7900004ef9988453240ab095de77662fabebd
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-14 09:31:11 +0200

    Special-Bonus
    C# While loops exercise

---
+++ While-loops-exercise/Special-bonus/Program.cs
@@ -0,0 +1,17 @@
+﻿int stopNumber = int.Parse(Console.ReadLine()); // число, което ако въведем спираме
+// повтаряме : въвеждаме числа -> проверка дали съвпада с stopNumber
+int previousNumber = stopNumber; // последното въведено число
+
+while(true)
+{
+    int number = int.Parse(Console.ReadLine()); // въведено число
+    if (number == stopNumber)
+    {
+        // 1. Увеличавам предното с 20 %
+        // 2. принтирам
+        // previousNumber + 20 % = previousNumber + 0.20 * previousNumber = 1.2 * previousNumber
+        Console.WriteLine(previousNumber * 1.2);
+        break;
+    }
+    previousNumber = number;
+}
