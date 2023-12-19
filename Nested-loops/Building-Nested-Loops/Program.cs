d15504aab26b4465e8fe8e1436e79f04d7053db3
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-19 21:21:07 +0200

    Building-Nested-loops
    Nested loops exercise SoftUni written on C#

---
+++ Nested-loops/Building-Nested-Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _4._Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floorCount = int.Parse(Console.ReadLine());
+            int roomCount = int.Parse(Console.ReadLine());
+
+            for (int floor = floorCount; floor >= 1; floor -= 1)
+            {
+                for (int room = 0; room < roomCount; room += 1)
+                {
+                    if (floor == floorCount)
+                    {
+                        Console.WriteLine($"L{floor}{room}");
+                    }
+                    else if (floor % 2 == 0)
+                    {
+                        Console.WriteLine($"O{ floor}{room}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"A{floor}{room}");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
