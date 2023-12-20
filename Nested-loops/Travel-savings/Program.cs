7b39ca2ab11efebb6f585ba3578d15ca72bf7588
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-20 10:42:32 +0200

    Travel-savings
    C# exercise nested loops - SoftUni course

---
+++ Nested-loops/Travel-savings/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _6._Travel_Savings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while (destination != "End")
+            {
+                double neededSum = double.Parse(Console.ReadLine());
+                double collectedSum = 0;
+                while (collectedSum < neededSum) 
+                {
+                    double sumToBeAdded = double.Parse(Console.ReadLine());
+                    collectedSum += sumToBeAdded;
+                    Console.WriteLine($"Collected: {collectedSum:F2}");
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
