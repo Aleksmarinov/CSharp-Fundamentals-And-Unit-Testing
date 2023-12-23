a48648f11273f478ab5fcc258b8ad136a5ae56c8
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-23 17:48:19 +0200

    Number-Pyramid

---
+++ Nested-loops/Number-Pyramid/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _5._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(number);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
