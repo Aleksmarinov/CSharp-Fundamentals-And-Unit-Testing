918ae017278f65ad37fcc5845973da8b69cd8ca5
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-25 10:12:25 +0200

    Sum-of-digits-calculator

---
+++ Nested-loops/Sum-of-digits-calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _7._Sum_of_Digits_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                int numberToProcess = int.Parse(command);
+                int sum = 0;
+
+                for (int number = numberToProcess; number > 0; number /= 10)
+                {
+                    int digit = number % 10;
+                    sum += digit;
+                }
+
+                Console.WriteLine($"Sum of digits = {sum}");
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
