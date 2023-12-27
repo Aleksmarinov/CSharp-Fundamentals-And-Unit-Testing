aa464f84d4047c3bd860457167a28347548ce806
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-27 09:39:51 +0200

    Prime-Numbers

---
+++ Nested-loops/Prime-numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _8._Prime_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = startNumber; currentNumber <= endNumber; currentNumber += 1)
+            {
+                bool isPrime = true;
+                int divider = 2;
+
+                while (divider < endNumber)
+                {
+                    if (currentNumber == divider)
+                    {
+                        divider += 1;
+                        continue;
+                    }
+
+                    if (currentNumber % divider == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                    divider += 1;
+                }
+
+                if (isPrime)
+                {
+                    Console.Write($"{currentNumber} ");
+                }
+            }
+        }
+    }
+}
