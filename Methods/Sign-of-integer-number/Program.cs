7a3ec6288c7b18720aa947b28662c7a473ece007
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-28 09:54:29 +0200

    Sign-of-integer-number
    C# exercise - methods

---
+++ Methods/Sign-of-integer-number/Program.cs
@@ -0,0 +1,19 @@
+﻿static void PrintNumberSign(int number)
+{
+    if (number == 0)
+    {
+        Console.WriteLine($"The number {number} is zero.");
+    }
+    else if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+}
+
+int number = int.Parse(Console.ReadLine());
+
+PrintNumberSign(number);
