6730dbe1682f567f0080ba52d9e4a70b14bae98a
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-29 08:59:12 +0200

    Grades - C# Methods exercise

---
+++ Methods/Grades/Program.cs
@@ -0,0 +1,27 @@
+﻿static void GradeInWords(double gradeParameter)
+{
+    if (gradeParameter >= 2 && gradeParameter < 3)
+    {
+        Console.WriteLine("Fail");
+    }
+    else if (gradeParameter >= 3 && gradeParameter < 3.5)
+        {
+        Console.WriteLine("Average");
+    }
+    else if (gradeParameter >= 3.5 && gradeParameter < 4.5)
+    {
+        Console.WriteLine("Good");
+    }
+    else if (gradeParameter >= 4.5 && gradeParameter < 5.5)
+    {
+        Console.WriteLine("Very good");
+    }
+    else
+    {
+        Console.WriteLine("Excellent");
+    }
+}
+
+double gradeFromUser = double.Parse(Console.ReadLine());
+
+GradeInWords(gradeFromUser);
