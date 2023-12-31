f088337993cd87165d572660cc67100bb8b76fca
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2023-12-31 09:45:56 +0200

    Printing-Triangle

---
+++ Methods/Printing-Triangle/Program.cs
@@ -0,0 +1,24 @@
+﻿static void PrintLine(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+
+static void PrintTriangle(int num)
+{
+    for(int currentLine = 1; currentLine <= num; currentLine++)
+    {
+        PrintLine(1, currentLine);
+    }
+    for (int currentLine = num - 1; currentLine >= 1; currentLine--)
+    {
+        PrintLine(1, currentLine);
+    }
+}
+
+int number = int.Parse(Console.ReadLine());
+
+PrintTriangle(number);
