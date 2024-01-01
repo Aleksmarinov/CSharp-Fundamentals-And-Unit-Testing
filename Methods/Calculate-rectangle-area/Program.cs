7c5b538ad646c5bc18bb3a1c29a2aa7c8e803163
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-01 10:25:30 +0200

    Calculate-rectangle-area

---
+++ Methods/Calculate-rectangle-area/Program.cs
@@ -0,0 +1,10 @@
+﻿static int CalculateRectangleArea(int width, int height)
+{
+    return width * height;
+}
+
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine()); 
+
+int area = CalculateRectangleArea(width, height);
+Console.WriteLine(area);
