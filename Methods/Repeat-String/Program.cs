3d185bdee0bdb0783dbfaeed6225b96ff3e87f8f
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-02 10:06:06 +0200

    Repeat-String

---
+++ Methods/Repeat-String/Program.cs
@@ -0,0 +1,16 @@
+﻿static string RepeatString(string text, int count)
+{
+    string output = "";
+    for (int i = 0; i < count; i++)
+    {
+        output += text;
+    }
+    return output;
+}
+
+string text = Console.ReadLine();
+int count = int.Parse(Console.ReadLine());
+
+string result = RepeatString(text, count);
+
+Console.WriteLine(result);
