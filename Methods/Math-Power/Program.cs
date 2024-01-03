c651221c3f5b42810992df5a029655adc82f70c9
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-03 16:53:37 +0200

    Math-Power

---
+++ Methods/Math-Power/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+int MathPower(int basePowerNum,int powerNum)
+{
+    int output = basePowerNum;
+    for (int i = 0; i < powerNum - 1; i++)
+    {
+        output *= basePowerNum;
+    }
+    return output;
+}
+
+int basePowerNum = int.Parse(Console.ReadLine());
+int powerNum = int.Parse(Console.ReadLine());
+
+int result = MathPower(basePowerNum, powerNum);
+Console.WriteLine(result);
