e9430ae5f61e2563a52a022082064c028097167f
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-05 09:51:33 +0200

    Multiply-Evens-By-Odds

---
+++ Methods/Mulitply-Even-By-Odd/Program.cs
@@ -0,0 +1,50 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+
+number = Math.Abs(number);
+
+int output = GetMultipleOfEvenAndOdds(number);
+
+Console.WriteLine(output);
+
+int GetMultipleOfEvenAndOdds(int number)
+{
+    int sumEven = GetSumOfEvenDigits(number);
+    int sumOdd = GetSumOfOddDigits(number);
+
+    int result = sumEven * sumOdd;
+
+    return result;
+}
+
+int GetSumOfEvenDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+
+        if (digit % 2 == 0)
+        {
+            sum += digit;
+        }
+    }
+    return sum;
+}
+
+int GetSumOfOddDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        number /= 10;
+
+        if (digit % 2 != 0)
+        {
+            sum += digit;
+        }
+    }
+    return sum;
+}
