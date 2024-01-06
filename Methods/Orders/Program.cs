74c650df311ca3d78a47f97bc77adc88e51973ba
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-06 10:29:26 +0200

    Orders

---
+++ Methods/Orders/Program.cs
@@ -0,0 +1,30 @@
+﻿string product = Console.ReadLine();
+int productQuantity = int.Parse(Console.ReadLine());
+
+double result = CalculatePrice(product, productQuantity);
+
+Console.WriteLine($"{result:F2}");
+
+static double CalculatePrice(string product, int productQuantity)
+{
+    double result = 0;
+
+    if (product == "coffee")
+    {
+        result = productQuantity * 1.5;
+    }
+    else if (product == "water")
+    {
+        result = productQuantity * 1.0;
+    }
+    else if (product == "coke")
+    {
+        result = productQuantity * 1.4;
+    }
+    else if (product == "snacks")
+    {
+        result = productQuantity * 2.0;
+    }
+
+    return result;
+}
