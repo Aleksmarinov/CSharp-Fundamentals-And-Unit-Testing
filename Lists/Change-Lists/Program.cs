0d722f45ff44d36b8f7c428b7f08546cf6edcb7c
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-07 12:08:18 +0200

    Change-Lists
    Lists exercise C#

---
+++ Lists/Change-Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿List<int> numbers = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToList();
+
+string command = Console.ReadLine();
+
+while (command != "end")
+{
+    string[] commandParts = command.Split(" ");
+    string commandName = commandParts[0];
+    int element = int.Parse(commandParts[1]);
+
+    if (commandName == "Delete")
+    {
+        numbers.RemoveAll(number => number == element);
+    }
+    else if (commandName == "Insert")
+    {
+        int position = int.Parse(commandParts[2]);
+        numbers.Insert(position, element);
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
