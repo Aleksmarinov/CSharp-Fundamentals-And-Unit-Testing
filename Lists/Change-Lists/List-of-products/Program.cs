0ba03589afa6a9c24f50c7f29d28a12f0647ef38
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-08 12:12:47 +0200

    List-of-products

---
+++ Lists/Change-Lists/List-of-products/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+List<string> products = new List<string>();
+
+for(int i = 1; i <= n; i++)
+{
+    string product = Console.ReadLine();
+    products.Add(product);
+}
+
+products.Sort();
+
+int number = 1;
+foreach(string product in products)
+{
+    Console.WriteLine(number + "." + product);
+    number++;
+}
