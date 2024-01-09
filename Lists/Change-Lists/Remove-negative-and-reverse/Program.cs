ef8f4922f7e789fd2e799c11c7770a0a6d2c9245
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-09 11:10:03 +0200

    Remove-negative-and-reverse

---
+++ Lists/Change-Lists/Remove-negative-and-reverse/Program.cs
@@ -0,0 +1,14 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+numbers.RemoveAll(number => number < 0);
+
+numbers.Reverse();
+
+if (numbers.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    Console.WriteLine(string.Join(" ", numbers));
+}
