1ecf172b98723093c2181307b291384d51c91387
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-10 09:47:28 +0200

    List-manipulation-basics

---
+++ Lists/Change-Lists/List-manipulation-basics/Program.cs
@@ -0,0 +1,34 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+string command = Console.ReadLine();
+
+while (command != "end")
+{
+    if (command.StartsWith("Add"))
+    {
+        int numberToAdd = int.Parse(command.Split(" ")[1]);
+        numbers.Add(numberToAdd);
+    }
+
+    else if (command.StartsWith("RemoveAt"))
+    {
+        int indexToRemove = int.Parse(command.Split(" ")[1]);
+        numbers.RemoveAt(indexToRemove);
+    }
+
+    else if (command.StartsWith("Remove"))
+    {
+        int numberToRemove = int.Parse(command.Split(" ")[1]);
+        numbers.Remove(numberToRemove);
+    }
+
+    else if (command.StartsWith("Insert"))
+    {
+        int numberToInsert = int.Parse(command.Split(" ")[1]);
+        int indexToInsert = int.Parse(command.Split(" ")[2]);
+        numbers.Insert(indexToInsert, numberToInsert);
+    }
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
