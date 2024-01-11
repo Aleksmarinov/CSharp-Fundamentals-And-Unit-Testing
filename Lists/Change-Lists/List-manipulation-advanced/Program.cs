b1ac5e55fa09c5ca7371be7d8fdf227dbcbec6b0
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-11 16:52:19 +0200

    List-Manipulation-Advanced

---
+++ Lists/Change-Lists/List-manipulation-advanced/Program.cs
@@ -0,0 +1,71 @@
+﻿List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+string command = Console.ReadLine();
+
+while (command != "end")
+{
+    if (command.StartsWith("Contains"))
+    {
+        int numbersContained = int.Parse(command.Split(" ")[1]);
+        if (numbers.Contains(numbersContained))
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No such number");
+        }
+    }
+
+    else if (command == "PrintEven")
+    {
+        foreach(int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                Console.Write(number + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    else if (command == "PrintOdd" )
+    {
+        foreach (int number in numbers)
+        {
+            if (number % 2 != 0)
+            {
+                Console.Write(number + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    else if (command == "GetSum")
+    {
+        Console.WriteLine(numbers.Sum());
+    }
+
+    else if (command.StartsWith("Filter"))
+    {
+        string condition = command.Split(" ")[1];
+        int number = int.Parse(command.Split(" ")[2]);
+        switch(condition)
+        {
+            case "<":
+                numbers.RemoveAll(numberInList => numberInList >= number);
+                break;
+            case ">":
+                numbers.RemoveAll(numberInList => numberInList <= number);
+                break;
+            case ">=":
+                numbers.RemoveAll(numberInList => numberInList > number);
+                break;
+            case "<=":
+                numbers.RemoveAll(numberInList => numberInList < number);
+                break;
+        }
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", numbers));
