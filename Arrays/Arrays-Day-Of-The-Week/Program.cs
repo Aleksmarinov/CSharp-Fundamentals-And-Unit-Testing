bc21564e5f79dbc01a22704ff49207cbf7894a33
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-12 14:32:22 +0200

    Day-Of-The-Week

---
+++ Arrays/Arrays-Day-Of-The-Week/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+if (n >= 1 && n <= 7)
+{
+    Console.WriteLine(days[n - 1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
