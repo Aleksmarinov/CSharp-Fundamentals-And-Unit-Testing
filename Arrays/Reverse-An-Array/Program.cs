64303ce4f9b2d9d4592b647a01858e181bfef89d
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-15 12:39:02 +0200

    Reverse-An-Array

---
+++ Arrays/Reverse-An-Array/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] numbers = new int[n];
+
+for(int index = 0; index <= numbers.Length - 1; index++)
+{
+    numbers[index] = int.Parse((Console.ReadLine()));
+}
+
+for(int index = numbers.Length - 1; index >= 0; index--)
+{
+    Console.Write(numbers[index] + " ");
+}
