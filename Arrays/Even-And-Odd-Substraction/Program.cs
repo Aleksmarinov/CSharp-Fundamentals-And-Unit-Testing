7bc4db772de36822d9b587e775092b1630fca703
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-16 12:27:03 +0200

    Even-And-Odd-Substraction

---
+++ Arrays/Even-And-Odd-Substraction/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+int sumEven = 0;
+int sumOdd = 0;
+
+foreach(int number in numbers)
+{
+    if(number % 2 == 0)
+    {
+        sumEven += number;
+    }
+    else
+    {
+        sumOdd += number;
+    }
+}
+
+Console.WriteLine(sumEven - sumOdd);
