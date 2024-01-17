7ea532d6459712b1a75e3e178ed0f8b06a97b484
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-17 16:13:01 +0200

    Equal-Arrays

---
+++ Arrays/Equal-Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] firstNumber = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+int[] secondNumber = Console.ReadLine()
+                     .Split(" ")
+                     .Select(int.Parse) 
+                     .ToArray();
+
+bool isIdentical = true;
+
+for(int index = 0; index <= firstNumber.Length - 1; index++)
+{
+    if (firstNumber[index] != secondNumber[index])
+    {
+        isIdentical = false;
+        Console.WriteLine("Arrays are not identical.");
+        break;
+    }
+}
+
+if (isIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
