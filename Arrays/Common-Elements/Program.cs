cd3507dbd1c6dcb89ad80f15bdbd9d65a4ee8224
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-18 11:34:07 +0200

    Common-Elements

---
+++ Arrays/Common-Elements/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] firstArray = Console.ReadLine() .Split(" ") .Select(int.Parse) .ToArray ();
+int[] secondArray = Console.ReadLine() .Split(" ") .Select(int.Parse) .ToArray ();
+
+
+foreach (int number in firstArray)
+{
+    foreach(int number2 in secondArray)
+    {
+        if (number == number2)
+        {
+            Console.Write(number + " ");
+            break;
+        }
+    }
+}
