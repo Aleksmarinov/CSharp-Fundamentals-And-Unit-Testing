aa1a73145be51a937a445d7e57bf9a6a259effba
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-21 11:06:53 +0200

    Merging-Lists

---
+++ Lists/Change-Lists/Merging-Lists/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> number1 = Console.ReadLine()
+                  .Split(" ")
+                  .Select(int.Parse)
+                  .ToList();
+
+List<int> number2 = Console.ReadLine()
+                  .Split(" ")
+                  .Select(int.Parse)
+                  .ToList();
+
+
+List<int> output = new List<int>();
+
+
+int longerCount = Math.Max(number1.Count, number2.Count);
+
+for (int i = 0; i < longerCount; i++)
+{
+    if (i < number1.Count)
+    {
+        output.Add(number1[i]);
+
+    }
+
+    if (i < number2.Count)
+    {
+        output.Add(number2[i]);
+    }
+}
+
+Console.WriteLine(string.Join(" ", output));
