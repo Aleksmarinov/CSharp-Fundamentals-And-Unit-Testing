8c8c540f3f9be8f847aa47d609d6362feb8becae
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-23 16:41:18 +0200

    Sum-Adjacent-Equal-Numbers

---
+++ Lists/Change-Lists/Sum-Adjacent-Equal-Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+for (int i = 0; i < input.Count - 1; i++)
+{
+    int num1 = input[i];
+    int num2 = input[i + 1];
+
+    if (num1 == num2)
+    {
+        input[i] = num1 + num2;
+        input.RemoveAt(i + 1);
+        i = -1;
+    }
+}
+
+
+Console.WriteLine(string.Join(" ", input));
