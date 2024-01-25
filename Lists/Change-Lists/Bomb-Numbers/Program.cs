9f3b73d59a859f409577d9f1854bc3ae1b18218f
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-25 15:36:29 +0200

    Bomb-Numbers

---
+++ Lists/Change-Lists/Bomb-Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿List<int> input = Console.ReadLine()
+                  .Split(" ")
+                  .Select(int.Parse)
+                  .ToList();
+
+List<int> bomb = Console.ReadLine()
+                  .Split(" ")
+                  .Select(int.Parse)
+                  .ToList();
+
+int bombNum = bomb[0];
+int bombPow = bomb[1];
+
+for (int i = 0; i < input.Count; i++)
+{
+    if (input[i] == bombNum)
+    {
+        int startIndex = i - bombPow;
+        int removeCount = bombPow + bombPow + 1;
+
+        if (startIndex < 0)
+        {
+            removeCount -= Math.Abs(startIndex);
+            startIndex = 0;
+        }
+
+        if (startIndex + removeCount > input.Count)
+        {
+            removeCount = input.Count - startIndex;
+        }
+
+        input.RemoveRange(startIndex, removeCount);
+        i = startIndex - 1;
+    }
+}
+int sum = input.Sum();
+Console.WriteLine(sum);
