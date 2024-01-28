1c441d2611b650fa537c5d691fc9a0abbe5f20b4
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-28 18:33:12 +0200

    Reverse-An-Array-Of-Strings

---
+++ Arrays/Reverse-An-Array-Of-Strings/Program.cs
@@ -0,0 +1,7 @@
+﻿//Console.WriteLine(string.Join(" ", Console.ReadLine().Split(' ').ToArray().Reverse().ToArray()));
+
+var array = Console.ReadLine().Split(' ').ToArray();
+List<string> reverseList = new List<string>(array);
+reverseList.Reverse();
+
+Console.WriteLine(string.Join(" ", reverseList));
