10d8e29e3ede71135faf113bb3f732a8c160caa7
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-30 15:43:52 +0200

    Rounding-Numbers

---
+++ Arrays/Rounding-Numbers/Program.cs
@@ -0,0 +1,8 @@
+﻿double[] numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+int[] arrays = new int[numbers.Length];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    arrays[i] = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+    Console.WriteLine($"{numbers[i]} => {arrays[i]}");
+}
