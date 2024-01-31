591e280520cde410094d35e82b19ebacf98a07f3
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-01-31 18:44:10 +0200

    Condence-Array-To-Number

---
+++ Arrays/Condence-Array-To-Number/Program.cs
@@ -0,0 +1,13 @@
+﻿var inputArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+var count = inputArray.Length;
+
+while(count > 1) 
+{
+    for (int i = 0; i < count - 1; i++)
+    {
+        inputArray[i] = inputArray[i] + inputArray[i + 1];
+    }
+    count--;
+}
+
+Console.WriteLine(inputArray[0]);
