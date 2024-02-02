746b1d2e2bd028accc0155e323099194e4843ea2
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-02-02 12:20:48 +0200

    Sum-Factorial-Even-Digits

---
+++ Methods/Sum-Factorial-Even-Digits/Program.cs
@@ -0,0 +1,33 @@
+﻿int result = int.Parse(Console.ReadLine());
+
+int totalResult = 0;
+
+while (result != 0)
+{
+    int digit = result % 10;
+
+    if (digit % 2 == 0)
+    {
+        int resultFactorial = CalculateFactorial(digit);
+        totalResult += resultFactorial;
+    }
+
+    result = result / 10;
+}
+
+Console.WriteLine(totalResult);
+static int CalculateFactorial(int number)
+{
+    int result = 1;
+
+    while (number > 0)
+    {
+        result = result * number;
+        number -= 1;
+    }
+
+    return result;
+}
+
+
+
