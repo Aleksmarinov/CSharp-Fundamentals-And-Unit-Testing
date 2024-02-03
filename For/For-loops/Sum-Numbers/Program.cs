c6f675ac955eb747c17d7e953ab4394ef9c7057b
Aleksandar Marinov <122726033+Aleksmarinov@users.noreply.github.com>
2024-02-03 16:35:49 +0200

    Sum-Numbers

---
+++ For/For-loops/Sum-Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = int.Parse(Console.ReadLine());
+double sum = 0;
+
+for (int i = 1; i <= number; i += 1)
+{
+    double sumNumbers = double.Parse(Console.ReadLine());
+    sum = sum + sumNumbers;
+}
+
+Console.WriteLine(sum);
